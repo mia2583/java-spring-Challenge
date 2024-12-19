32ad695f543b81d4e7ff1981bb9a6db1dcf61c87
mia2583 <mia2583@pusan.ac.kr>
2024-12-19 19:40:12 +0900

    [feat] 회원 도메인 개발
    회원 리포지토리, 서비스 개발
    회원 기능 테스트 작성

---
+++ jpashop/src/main/java/jpastudy/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package jpastudy.jpashop.service;
+
+import jpastudy.jpashop.domain.Member;
+import jpastudy.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 조회에 최적화
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
